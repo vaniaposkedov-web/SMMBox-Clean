2bb21d236b3dba54c3d29aae245b541709daf6cf
Ivan Poskedov <vania.poskedov@gmail.com>
2026-03-07 14:48:33 +0500

    настройка нейронки 2в

--- backend/src/index.js
+++ backend/src/index.js
@@ -22,6 +22,8 @@ const allowedOrigins = [
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.set('trust proxy', 1);
+
 app.use(cors({ 
   origin: allowedOrigins, 
   credentials: true 
